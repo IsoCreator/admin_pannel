a4496b6571ef918b4d6fc69fe93d7152502263c6
IsoCreator <36802883+IsoCreator@users.noreply.github.com>
2018-04-07 02:18:56 +0200

    Add files via upload
    Again fixed delete.php.

--- delete.php
+++ delete.php
@@ -1,6 +1,9 @@
 <?php
 	include('connect_db.php');
 	
+	// get the 'id' variable from the URL
+	$id = $_GET['id'];
+
 	// delete record from database
 	$sql = "DELETE FROM form_data WHERE id = $id";
 	$conn->query($sql);
