bb135366b0f8d2e7260841d9e1e56cdf6a102ab2
IsoCreator <36802883+IsoCreator@users.noreply.github.com>
2018-05-13 00:46:44 +0200

    Added 'request_method'

--- register.php
+++ register.php
@@ -3,10 +3,19 @@
 	
 	$reg_username = $POST["reg_db_username"];
 	$reg_password = $POST["reg_db_password"];
-	$reg_email = $POST["reg_email"];
+	$reg_email = $POST["reg_db_email"];
 	
 	$sql = "INSERT INTO reg_data (reg_db_username, reg_db_password, reg_db_email)
 			VALUES ('$reg_username', '$reg_password', '$reg_email')";
+	
+	if($_SERVER['REQUEST_METHOD'] == 'POST')
+	{
+		if ($conn->query($sql) === TRUE) {
+		echo "New record created successfully" . "<br>";
+		} else {
+			echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
 	
 	$conn->close();
 	
