64d0bdbf0aef3941a2251edf46ac9009a19cd742
IsoCreator <36802883+IsoCreator@users.noreply.github.com>
2018-05-13 00:37:34 +0200

    Add files via upload

--- register.php
+++ register.php
@@ -1,12 +1,12 @@
 <?php
 	include('connect_db.php');
 	
-	$username = $POST["reg_username"];
-	$password = $POST["reg_password"];
-	$email = $POST["reg_email"];
+	$reg_username = $POST["reg_db_username"];
+	$reg_password = $POST["reg_db_password"];
+	$reg_email = $POST["reg_email"];
 	
-	$sql = "INSERT INTO reg_data (reg_username, reg_password, reg_email)
-			VALUES ('$username', '$password', '$email')";
+	$sql = "INSERT INTO reg_data (reg_db_username, reg_db_password, reg_db_email)
+			VALUES ('$reg_username', '$reg_password', '$reg_email')";
 	
 	$conn->close();
 	
